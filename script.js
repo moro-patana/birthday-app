7cb633e5de71bff39f0cc8020be0de9703a2deaf
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-09-16 16:33:15 +0300

    put everything inside the fetchPerson function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// import "regenerator-runtime/runtime";
-
+const endpoint = "https://date-fns.org/v1.29.0/docs/differenceInYears";
 const basepoint = './people.json';
 function wait(ms = 0) {
 	return new Promise(resolve => setTimeout(resolve, ms));
@@ -12,189 +11,197 @@ async function destroyPopup(popup) {
 	popup.remove();
 	popup = null;
 }
-
 async function fetchPerson() {
     const response = await fetch(basepoint);
     const data = await response.json();
-    return data;
-    }
-async function displayList(){
-        const personList = await fetchPerson();
-        tbody.innerHTML = personList
-            .map(person => {
-                    function nth(day) {
-                        if (day > 3 && day < 21) return 'th';
-                        switch (day % 10) {
-                          case 1:  return "st";
-                          case 2:  return "nd";
-                          case 3:  return "rd";
-                          default: return "th";
-                        }
-                      }
-                      let current_datetime = new Date(person.birthday);
-                      const date = current_datetime.getDate();
-                      const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][current_datetime.getMonth()];
-                        let today = new Date();
-                        let age = today.getFullYear() - current_datetime.getFullYear();
-                    
-                    return `
-                    <tr data-id="${person.id}">
-                        <th scope="row"><img src="${person.picture}" alt="${person.firstName + ' ' + person.lastName}"/></th>
-                        <td class="text-white">${person.lastName}</td>
-                        <td class="text-white">${person.firstName}</td>
-                        <td class="text-white">Turns ${age} on ${month} ${date}<sup>${nth(date)}</sup></td>
-                        <td class="text-white"> Days</td>
-                        <td class="text-white">
-                            <button type="button" class="edit btn bg-warning" value="${person.id}">
-                              Edit
-                            </button>
-                        </td>
-                        <td class="text-white">
-                            <button type="button" class="delete btn bg-warning" value="${person.id}">
-                                Delete
-                            </button>
-                        </td>
-                    </tr>`
-            })
-            .join('');
-    };
-
-    const editPerson = (e) => {
-        const editBtn = e.target.closest('button.edit');
-        if (editBtn) {
-            e.preventDefault();
-            const findTr = e.target.closest('tr');
-            const btn = findTr.querySelector('.edit')
-            const id = btn.value;
-            editPopup(id);
-        }
-    }
-    // Create an form html to edit the parteners profile
-    const editPopup = (id) => {
-        return new Promise(async function (resolve, reject) {
-            const personList = await fetchPerson();
-            const person = personList.find(person => person.id === id);
-            console.log(id);
-            const popup = document.createElement(`form`);
-            popup.classList.add('popup');
-            popup.innerHTML = `
-                <div class="form-input">
-                    <fieldset>
-                        <label for="lastName">Lastname</label>
-                        <input
-                            type="text"
-                            required
-                            name="lastName"
-                            id="lastName"
-                            value
-                            ="${person.lastName}"
-                        />
-                    </fieldset>
-                    <fieldset>
-                    <label for="firstName">Firstname</label>
-                    <input
-                        type="text"
-                        required
-                        name="firstName"
-                        id="firstName"
-                        value
-                        ="${person.firstName}"
-                    />
-                    </fieldset>
-                    <fieldset>
-                    <label for="birthday">Birthday</label>
-                    <input
-                        type="text"
-                        required
-                        name="birthday"
-                        id="birthday"
-                        value
-                        ="${person.birthday}"
-                    />
-                    </fieldset>
-                    <div class="button">
-                    <button type="submit" class="btn btn-danger save">Save</button>
-                    <button type="button" class="btn btn-danger cancel">Cancel</button>
-                    </div>
-                </div>
-`;
-            // Create skip button and appendchild it to the popup
-            window.addEventListener('click', e => {
-                if (e.target.closest('button.cancel')) {
-                    destroyPopup(popup);
-                }
-            })
-    
-            // Listen to the submit button to save the changes
-            popup.addEventListener('submit', (e) => {
-                e.preventDefault();
-                const popup = e.target;
-                resolve(popup.value);
-                // const form = e.target;
-                person.lastName = popup.lastName.value;
-                person.firstName = popup.firstName.value;
-                person.birthday = popup.birthday.value;
-                person.id = popup.id.value;
-
-                destroyPopup(popup);
-            },
-                { once: true }
-            )
-            document.body.appendChild(popup);
-            await wait(50);
-            popup.classList.add('open');
+    const storedHTML = (personList) => {
+        return personList
+        .map(person => {
+                function nth(day) {
+                    if (day > 3 && day < 21) return 'th';
+                    switch (day % 10) {
+                      case 1:  return "st";
+                      case 2:  return "nd";
+                      case 3:  return "rd";
+                      default: return "th";
+                    }
+                  }
+                  let current_datetime = new Date(person.birthday);
+                  const date = current_datetime.getDate();
+                  const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][current_datetime.getMonth()];
+                    let today = new Date();
+                    let age = today.getFullYear() - current_datetime.getFullYear();
+                
+                return `<tr data-id="${person.id}">
+                    <th scope="row"><img src="${person.picture}" alt="${person.firstName + ' ' + person.lastName}"/></th>
+                    <td class="text-white">${person.lastName}</td>
+                    <td class="text-white">${person.firstName}</td>
+                    <td class="text-white">Turns ${age} on ${month} ${date}<sup>${nth(date)}</sup></td>
+                    <td class="text-white"> Days</td>
+                    <td class="text-white">
+                        <button type="button" class="edit btn bg-warning" value="${person.id}">
+                          Edit
+                        </button>
+                    </td>
+                    <td class="text-white">
+                        <button type="button" class="delete btn bg-warning" value="${person.id}">
+                            Delete
+                        </button>
+                    </td>
+                </tr>`
         })
+        .join('');
     }
-    
-    const deletePerson = (e) => {
-        const deleteBtn = e.target.closest('button.delete');
-        if (deleteBtn) {
-            const tableRow = e.target.closest('tr');
-            e.preventDefault();
-            const id = tableRow.dataset.id;
-            deletePopup(id);
-        }
-    
+     function displayList(){
+         const html = storedHTML(data);
+        tbody.innerHTML = html;
     };
+    displayList();
+
+//     const editPerson = (e) => {
+//         const editBtn = e.target.closest('button.edit');
+//         if (editBtn) {
+//             e.preventDefault();
+//             const findTr = e.target.closest('tr');
+//             const btn = findTr.querySelector('.edit')
+//             const id = btn.value;
+//             editPopup(id);
+//         }
+//     }
+//     // Create an form html to edit the parteners profile
+//     const editPopup = (id) => {
+//         return new Promise(async function (resolve, reject) {
+//             const person = personList.find(person => person.id === id);
+//             console.log(id);
+//             const popup = document.createElement(`form`);
+//             popup.classList.add('popup');
+//             popup.innerHTML = `
+//                 <div class="form-input">
+//                     <fieldset>
+//                         <label for="lastName">Lastname</label>
+//                         <input
+//                             type="text"
+//                             required
+//                             name="lastName"
+//                             id="lastName"
+//                             value
+//                             ="${person.lastName}"
+//                         />
+//                     </fieldset>
+//                     <fieldset>
+//                     <label for="firstName">Firstname</label>
+//                     <input
+//                         type="text"
+//                         required
+//                         name="firstName"
+//                         id="firstName"
+//                         value
+//                         ="${person.firstName}"
+//                     />
+//                     </fieldset>
+//                     <fieldset>
+//                     <label for="birthday">Birthday</label>
+//                     <input
+//                         type="text"
+//                         required
+//                         name="birthday"
+//                         id="birthday"
+//                         value
+//                         ="${person.birthday}"
+//                     />
+//                     </fieldset>
+//                     <div class="button">
+//                     <button type="submit" class="btn btn-danger save">Save</button>
+//                     <button type="button" class="btn btn-danger cancel">Cancel</button>
+//                     </div>
+//                 </div>
+// `;
+//             // Create skip button and appendchild it to the popup
+//             window.addEventListener('click', e => {
+//                 if (e.target.closest('button.cancel')) {
+//                     destroyPopup(popup);
+//                 }
+//             })
+    
+//             // Listen to the submit button to save the changes
+//             popup.addEventListener('submit', (e) => {
+//                 e.preventDefault();
+//                 const popup = e.target;
+//                 resolve(popup.value);
+//                 // const form = e.target;
+//                 const newp = {
+//                 lastName : popup.lastName.value,
+//                 firstName : popup.firstName.value,
+//                 birthday : popup.birthday.value,
+//                 id : popup.id.value,
+//                 }
+                
+
+//                 person.firstName = newp.firstName
+//                 console.log(person)
+//                 destroyPopup(popup);
+                 
+//                 displayList();
+//             },
+//                 { once: true }
+//             )
+//             document.body.appendChild(popup);
+//             await wait(50);
+//             popup.classList.add('open');
+//         })
+//     }
+    
+//     const deletePerson = (e) => {
+//         const deleteBtn = e.target.closest('button.delete');
+//         if (deleteBtn) {
+//             const tableRow = e.target.closest('tr');
+//             e.preventDefault();
+//             const id = tableRow.dataset.id;
+//             deletePopup(id);
+//         }
+    
+//     };
     
     
-const deletePopup = (id) => {
-        // create confirmation popup here
-        console.log(id);
-        return new Promise(async function (resolve) {
-            const personList = await fetchPerson();
-            const modal = document.createElement('div');
-            modal.classList.add('modal');
-            modal.innerHTML = `
-            <p class="confirm">Do you want to delete?</p>
-            <div class="confirm-btn">
-                <button class="btn btn-primary yes">Yes</buton>
-                <button class="btn btn-primary no">No</buton>
-            </div>
-            `;
-            document.body.appendChild(modal);
-            await wait(50);
-            modal.classList.add('open');
+// const deletePopup = (id) => {
+//         // create confirmation popup here
+//         console.log(id);
+//         return new Promise(async function (resolve) {
+//             const modal = document.createElement('div');
+//             modal.classList.add('modal');
+//             modal.innerHTML = `
+//             <p class="confirm">Do you want to delete?</p>
+//             <div class="confirm-btn">
+//                 <button class="btn btn-primary yes">Yes</buton>
+//                 <button class="btn btn-primary no">No</buton>
+//             </div>
+//             `;
+//             document.body.appendChild(modal);
+//             await wait(50);
+//             modal.classList.add('open');
             
-            modal.addEventListener('click', (e) => {
-                if (e.target.closest('button.yes')) {
-                    const personToDelete = personList.filter(person => person.id !== id);
-                    persons = personToDelete;
-                    displayList(personToDelete);
-                    modal.classList.remove("open");
-                    destroyPopup(modal)
+//             modal.addEventListener('click', (e) => {
+//                 if (e.target.closest('button.yes')) {
+//                     const personToDelete = personList.filter(person => person.id !== id);
+//                     persons = personToDelete;
+//                     displayList(personToDelete);
+//                     modal.classList.remove("open");
+//                     destroyPopup(modal)
     
-                }
-            });
-            window.addEventListener('click', e => {
-                if (e.target.closest('button.no')) {
-                    destroyPopup(modal);
-                }
-            })
-        })
-    }
+//                 }
+//             });
+//             window.addEventListener('click', e => {
+//                 if (e.target.closest('button.no')) {
+//                     destroyPopup(modal);
+//                 }
+//             })
+//         })
+//     }
     
- tbody.addEventListener('click', editPerson);
- tbody.addEventListener('click', deletePerson);
+//  tbody.addEventListener('click', editPerson);
+//  tbody.addEventListener('click', deletePerson);
  
-displayList();
+
+
+    }
+fetchPerson();
